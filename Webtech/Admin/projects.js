790ea5a530db2292572905bbf7902858117ff49c
afriyierodney64 <150020106+afriyierodney64@users.noreply.github.com>
2026-01-28 10:16:44 +0000

    Delete Webtech directory

--- Webtech/Admin/projects.js
+++
@@ -1,9 +0,0 @@
-  function openPreview(id) {
-    const modal = document.getElementById(id);
-    modal.classList.add("active");
-  }
-
-  function closePreview(id) {
-    const modal = document.getElementById(id);
-    modal.classList.remove("active");
-  }
